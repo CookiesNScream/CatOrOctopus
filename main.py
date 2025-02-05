af3161f90eefd8dd6950bf0af459fd3ac77df439
Arya Venkatesan <ar6165@gmail.com>
2025-02-04 19:46:06 -0500

    further testing

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ training_data, test_data = prepare_data() # should return into two variables or 
 model = construct_model() #could take params or not idk
 
 history = train_model(model, training_data, test_data, 10)
+ 
